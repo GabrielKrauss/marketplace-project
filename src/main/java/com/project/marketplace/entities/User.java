d22209516e9e5935c094b58e3f4dc552142b4197
Gabriel Krauss <gabrielkrauscosta@gmail.com>
2024-03-16 17:37:44 -0300

    User entity and resources

---
+++ src/main/java/com/project/marketplace/entities/User.java
@@ -0,0 +1,54 @@
+package com.project.marketplace.entities;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private int id;
+	private String email;
+	private String password;
+	
+	
+	public User() {
+	}
+
+	public User(int id, String email, String password) {
+		super();
+		this.id = id;
+		this.email = email;
+		this.password = password;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
+}
---
+++ src/main/java/com/project/marketplace/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.project.marketplace.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.marketplace.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(15, "gabrielkrauscosta@gmail.com", "luanalinda");
+		return ResponseEntity.ok().body(u);
+	}
+}
