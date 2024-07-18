ac723cc28657ba746aa806ed02d457dd14b79cf2
Gabriel Krauss <gabrielkrauscosta@gmail.com>
2024-07-18 17:51:58 -0300

    User delete and update, exception handling

--- src/main/java/com/project/marketplace/services/UserService.java
+++ src/main/java/com/project/marketplace/services/UserService.java
@@ -11,6 +11,7 @@ import com.project.marketplace.entities.Role;
 import com.project.marketplace.entities.User;
 import com.project.marketplace.repositories.RoleRepository;
 import com.project.marketplace.repositories.UserRepository;
+import com.project.marketplace.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -27,7 +28,7 @@ public class UserService {
 
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 
 	public User insert(User obj) {
@@ -37,5 +38,20 @@ public class UserService {
 			obj.setRoles(roles);
 		}
 		return repository.save(obj);
+	}
+	
+	public void delete(Long id) {
+		repository.deleteById(id);
+	}
+	
+	public User update(Long id, User obj) {
+		User entity = repository.getReferenceById(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setEmail(obj.getEmail());
+		// Adicionar outros campos se precisar
 	}
 }
